13245574817d4d3649954bab16c759bc4f46de93
kimyounghee425 <doctorsean0425@gmail.com>
2025-10-10 21:48:19 +0900

    [chore] 배포 시점에 msw 활성화

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,9 +3,16 @@ import { createRoot } from 'react-dom/client';
 import AppRouter from './router/AppRouter';
 import '../global.css';
 
-if (process.env.NODE_ENV === 'development') {
+// if (process.env.NODE_ENV === 'development') {
+//   const { worker } = await import('@/mocks/browers');
+//   worker.start();
+// }
+
+if (import.meta.env.VITE_MSW_ENABLED === 'true') {
   const { worker } = await import('@/mocks/browers');
-  worker.start();
+  await worker.start({
+    onUnhandledRequest: 'bypass',
+  });
 }
 
 createRoot(document.getElementById('root')!).render(
