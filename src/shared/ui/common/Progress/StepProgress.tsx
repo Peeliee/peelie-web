3ceb3a8574b59515eaebf8c59934c6fb0a2ee481
kimyounghee425 <doctorsean0425@gmail.com>
2025-11-06 14:59:58 +0900

    reset

--- src/shared/ui/common/Progress/StepProgress.tsx
+++ src/shared/ui/common/Progress/StepProgress.tsx
@@ -4,6 +4,7 @@ interface StepProgressProps {
   currentStep: number;
 }
 
+// TODO : 색상 알맞게 수정하기 (현재는 프로그레스가 작아도 모든 색상이 다뜸)
 export const StepProgress = ({ currentStep }: StepProgressProps) => {
   const stepColors = [
     'var(--color-peelie-primary-900)',
@@ -11,43 +12,41 @@ export const StepProgress = ({ currentStep }: StepProgressProps) => {
     'var(--color-peelie-positive-600)',
     'var(--color-peelie-secondary-200)',
   ];
-
   const labels = ['Step 1', 'Step 2', 'Step 3', 'Step 4'];
-
-  const base = 10;
-  const progressPercent = Math.min(
-    100,
-    base + ((currentStep - 1) / (labels.length - 1)) * (100 - base),
-  );
+  const progressMap = [25, 60, 80, 100];
+  const progressPercent = progressMap[currentStep - 1];
 
   return (
     <div className="w-full">
-      {/* 바 */}
       <div className="relative h-1.5 w-full bg-gray-200 rounded-full overflow-hidden">
         <div
           className="absolute inset-0 h-full rounded-full"
           style={{
+            width: `${progressPercent}%`,
             background: `
-              linear-gradient(
+                linear-gradient(
                 to right,
-                ${stepColors[0]} 0%,
-                ${stepColors[1]} 33%,
-                ${stepColors[2]} 66%,
-                ${stepColors[3]} 100%
-              )
+                var(--color-peelie-primary-900) 0%,
+                var(--color-peelie-primary-900) 10%,
+                var(--color-peelie-primary-600) 30%,
+                var(--color-peelie-primary-600) 40%,
+                var(--color-peelie-positive-600) 60%,
+                var(--color-peelie-positive-600) 65%,
+                var(--color-peelie-secondary-200) 85%,
+                var(--color-peelie-secondary-200) 100%
+                )
             `,
-            clipPath: `inset(0 ${100 - progressPercent}% 0 0 round 999px)`,
-            transition: 'clip-path 0.4s ease',
+            maskImage: `linear-gradient(to right, black ${progressPercent}%, transparent ${progressPercent}%)`,
+            WebkitMaskImage: `linear-gradient(to right, black ${progressPercent}%, transparent ${progressPercent}%)`,
           }}
         />
       </div>
 
-      {/* 라벨 */}
       <div className="flex justify-between mt-2 detail-regular">
         {labels.map((label, i) => (
           <div key={i} className="flex flex-col items-center gap-1">
             <div
-              className="w-1 h-1 rounded-full"
+              className="w-1 h-1 rounded-full transition-colors"
               style={{
                 backgroundColor:
                   i + 1 <= currentStep ? stepColors[i] : 'var(--color-peelie-gray-200)',
