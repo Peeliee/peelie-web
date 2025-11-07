e90f7a9f916dbe70e6a26bc78b3811aecb1e5fa1
kimyounghee425 <doctorsean0425@gmail.com>
2025-11-08 02:28:44 +0900

    [feat] ios 하단 패딩 설정

--- apps/peelie-app/app/index.tsx
+++ apps/peelie-app/app/index.tsx
@@ -1,7 +1,7 @@
 // app/index.tsx
 import { WebView } from "react-native-webview";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { View } from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { StyleSheet, Platform } from "react-native";
 
 export default function HomeScreen() {
     const DEV_URL = "http://10.221.43.192:5173";
@@ -9,25 +9,41 @@ export default function HomeScreen() {
     const sourceUrl = __DEV__ ? DEV_URL : PROD_URL;
 
     return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
-            <WebView
-                source={{ uri: "http://172.30.1.89:5173/" }}
-                originWhitelist={["*"]}
-                allowsInlineMediaPlayback
-                startInLoadingState
-                javaScriptEnabled
-                domStorageEnabled
-                mixedContentMode="always"
-                style={{ flex: 1 }}
-                onLoadStart={() => console.log("WebView 시작")}
-                onLoadEnd={() => console.log("WebView 완료")}
-                onError={(e) => console.log("WebView 오류:", e.nativeEvent)}
-                onMessage={(event) => console.log("📩 message:", event.nativeEvent.data)}
-                injectedJavaScript={`
+        <SafeAreaProvider>
+            <SafeAreaView
+                style={[
+                    styles.container,
+                    {
+                        paddingBottom: Platform.OS === "ios" ? -20 : 0,
+                    },
+                ]}
+            >
+                <WebView
+                    source={{ uri: "http://172.30.1.89:5173/" }}
+                    originWhitelist={["*"]}
+                    allowsInlineMediaPlayback
+                    startInLoadingState
+                    javaScriptEnabled
+                    domStorageEnabled
+                    mixedContentMode="always"
+                    style={{ flex: 1 }}
+                    onLoadStart={() => console.log("WebView 시작")}
+                    onLoadEnd={() => console.log("WebView 완료")}
+                    onError={(e) => console.log("WebView 오류:", e.nativeEvent)}
+                    onMessage={(event) => console.log("📩 message:", event.nativeEvent.data)}
+                    injectedJavaScript={`
           window.ReactNativeWebView.postMessage(document.title);
           true;
         `}
-            />
-        </SafeAreaView>
+                />
+            </SafeAreaView>
+        </SafeAreaProvider>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "white",
+    },
+});
