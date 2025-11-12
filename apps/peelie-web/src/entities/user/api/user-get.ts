3529782b7f3263f963033ca347d272927ff3e917
kimyounghee425 <doctorsean0425@gmail.com>
2025-11-12 19:39:51 +0900

    [feat] user get api 핸들러작성, 목데이터 생성

--- apps/peelie-web/src/entities/user/api/user-get.ts
+++ apps/peelie-web/src/entities/user/api/user-get.ts
@@ -1,10 +1,14 @@
 import api from '@/shared/api/ky';
 
-import { type StepInfoCardResponseDTO } from '../model/user.type';
+import type { StepInfoCardResponseDTO, UserResponseDTO } from '../model/user.type';
 
 export const userGet = {
   getUserStepInfo: async (): Promise<StepInfoCardResponseDTO> => {
     const response = await api.get('onboarding/card/status').json<StepInfoCardResponseDTO>();
+    return response;
+  },
+  getUserInfo: async (): Promise<UserResponseDTO> => {
+    const response = await api.get('profile').json<UserResponseDTO>();
     return response;
   },
 };
---
+++ apps/peelie-web/src/mocks/handlers/userHandlers.ts
@@ -0,0 +1,23 @@
+import { http, HttpResponse } from 'msw';
+
+import type { UserResponseDTO } from '@/entities/user/model/user.type';
+import { type ApiErrorMessage } from '@/shared/api/types';
+import { UserMock } from '../data/user';
+
+const USER_API_PREFIX = `${import.meta.env.VITE_API_BASE_URL}/api/v1`;
+
+export const userHandlers = [
+  http.get<never, UserResponseDTO, UserResponseDTO | ApiErrorMessage>(
+    `${USER_API_PREFIX}/profile`,
+    async () => {
+      await new Promise((resolve) => setTimeout(resolve, UserMock.delay));
+
+      return HttpResponse.json({
+        status: 200,
+        success: true,
+        message: 'OK',
+        data: UserMock.data,
+      });
+    },
+  ),
+];
