4c1403c078bd3dfff3bfb5e9c1b3be2e97f39c6f
kimyounghee425 <doctorsean0425@gmail.com>
2025-12-03 09:55:13 +0900

    [feat] 인트로 페이지 텍스트 추가

--- apps/peelie-web/src/pages/Onboarding/ui/IntroducePeeliePage.tsx
+++ apps/peelie-web/src/pages/Onboarding/ui/IntroducePeeliePage.tsx
@@ -7,17 +7,27 @@ import { Button } from '@/shared/ui/common/button';
 
 import MockImg from '@/assets/mockImg.svg?react';
 
-const INTRO_TEXTS = [
-  `처음 만난 사람 앞에서 
-   얼마나 나를 알려줄 지 망설였다면 
-   Peelie 는 그 마음부터 함께 할게요`,
-
-  `가까워질수록 
-   나의 마음도 조금씩 드러나요. 
-   Peelie 는 그 흐름을 따라 함께 움직여요`,
-
-  `부담 없이, 당신의 속도에 맞게 
-   천천히 당신의 리듬으로 시작해볼까요? \n\n`,
+const INTRO_CONTENT = [
+  {
+    main: `교류 성향 캐릭터를 선택하고,
+    나에게 맞는 방식으로 친구와 가까워져 봐요`,
+    sub: `Peelie는 교류 성향 캐릭터 설정을 통해,
+          사용자에게 맞는 교류 속도로 관계를 시작할 수 있도록 도와줘요.`,
+  },
+  {
+    main: `퀴즈를 통해 정보를 하나씩 공개하며,
+          자연스럽게 가까워져요`,
+    sub: `Peelie의 교류 퀴즈로 단계별로 하나씩 풀어가며,
+          친해지고 싶었던 친구와의 교류를 시작해봐요.`,
+  },
+  {
+    main: `단계별 퀴즈를 풀며 얻은 보상으로
+          친구의 개인적인 정보와 이야기까지
+          가까워져 봐요`,
+    sub: `Peelie단 단계별 퀴즈를 풀수록 새로운 보상이 열리고,
+          보상을 통해 친구의 취향과 이야기, sns정보를 하나씩 알아갈 수
+          있어요.`,
+  },
 ];
 
 interface IntroducePeeliePageProps {
@@ -40,14 +50,27 @@ const IntroducePeeliePage = ({ onNext }: IntroducePeeliePageProps) => {
       <div className="flex flex-col w-full justify-start">
         <AnimatePresence mode="wait">
           <motion.p
-            key={index}
+            key={`main-${index}`}
             className="heading-3-medium mt-10 ml-4 whitespace-pre-line"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ duration: 0.2 }}
           >
-            {INTRO_TEXTS[index]}
+            {INTRO_CONTENT[index].main}
+          </motion.p>
+        </AnimatePresence>
+
+        <AnimatePresence mode="wait">
+          <motion.p
+            key={`sub-${index}`}
+            className="body-2-regular text-gray-400 mt-3 ml-4 whitespace-pre-line"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.2 }}
+          >
+            {INTRO_CONTENT[index].sub}
           </motion.p>
         </AnimatePresence>
       </div>
