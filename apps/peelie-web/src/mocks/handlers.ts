9ae6b03c17d6e4f84173d4380479a42ce2883aad
kimyounghee425 <doctorsean0425@gmail.com>
2026-05-02 13:12:09 +0900

    ⚰️ Chore : 미사용 코드 삭제

--- apps/peelie-web/src/mocks/handlers.ts
+++ apps/peelie-web/src/mocks/handlers.ts
@@ -1,15 +1,3 @@
-import { categoryHandlers } from './handlers/categoryHandlers';
-import { onboardingHandlers } from './handlers/onboardingHandlers';
-import { friendHandlers } from './handlers/friendHandlers';
-import { userHandlers } from './handlers/userHandlers';
-import { quizHandlers } from './handlers/quizHandlers';
 import { aiChatHandlers } from './handlers/aiChatHandlers';
 
-export const handlers = [
-  ...categoryHandlers,
-  ...onboardingHandlers,
-  ...friendHandlers,
-  ...userHandlers,
-  ...quizHandlers,
-  ...aiChatHandlers,
-];
+export const handlers = [...aiChatHandlers];
